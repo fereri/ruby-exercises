553a089b0ed3de9e47a84a3ad1373110b1121700
David <250323761+fereri@users.noreply.github.com>
2026-06-27 15:49:19 +0200

    Almost all of exercise nested array is done.

--- ruby_basics/12_nested_collections/exercises/nested_array_exercises.rb
+++ ruby_basics/12_nested_collections/exercises/nested_array_exercises.rb
@@ -2,6 +2,8 @@ def blank_seating_chart(number_of_rows, seats_per_row)
   # return a 2d array to represent a seating chart that contains
   # number_of_rows nested arrays, each with seats_per_row entries of nil to
   # represent that each seat is empty.
+  seating = Array.new(number_of_rows) {Array.new(seats_per_row)}
+  return seating
 
   # Example: blank_seating_chart(2, 3) should return:
   # [
@@ -16,16 +18,22 @@ end
 def add_seat_to_row(chart, row_index, seat_to_add)
   # take a chart (2d array)  and add seat_to_add to the end of the row that is
   # at row_index index of the chart, then return the chart
+  chart[row_index].push(seat_to_add)
+  return chart
 end
 
 def add_another_row(chart, row_to_add)
   # take a chart and add row_to_add to the end of the chart,
   # then return the chart.
+  chart.push(row_to_add)
+  return chart
 end
 
 def delete_seat_from_row(chart, row_index, seat_index)
   # take a chart and delete the seat at seat_index of the row at row_index of
   # the chart, then return the chart
+  chart[row_index].delete_at(seat_index)
+  return chart
 
   # Hint: explore the ruby docs to find a method for deleting from an array!
 end
@@ -33,11 +41,14 @@ end
 def delete_row_from_chart(chart, row_index)
   # take a chart and delete the row at row_index of the chart,
   # then return the chart
+  chart.delete_at(row_index)
+  return chart
 end
 
 def count_empty_seats(chart)
   # take a chart and return the number of empty (nil) seats in it
-
+  empty = chart.flatten.count(nil)
+  return empty
   # NOTE: `chart` should **not** be mutated
 end
 
